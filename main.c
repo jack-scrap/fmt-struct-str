3b2378b5778ca53e715d73fe1537260a55598b14
JackHasaKeyboard <jackhasakeyboard@gmail.com>
2024-02-23 21:23:50 -0700

    Output string enclosed in double quotes

--- main.c
+++ main.c
@@ -1,3 +1,26 @@
+#include <stdio.h>
+
+const char wrap = '"';
+
+void err(char* msg) {
+	fprintf(stderr, "Error: %s\n", msg);
+}
+
 int main(int argc, char* argv[]) {
+	if (argc == 1) {
+		err("No arguments");
+
+		return 1;
+	}
+
+	if (argc != 1 + 1) {
+		err("Wrong number of arguments");
+
+		return 1;
+	}
+
+	char* str = argv[1];
+	printf("%c%s%c", wrap, str, wrap);
+
 	return 0;
 }
